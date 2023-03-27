18c427ab272ff4c8e45fa948c3bdc040a228c09b
Pankaj Jangir <pankajjangir20@cse.iiitp.ac.in>
2023-03-27 20:51:05 +0530

    app

--- server/index.js
+++ server/index.js
@@ -11,21 +11,22 @@ const port = config.port;
 
 const app = Express();
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
 app.use(
   cors({
     origin: config.origin,
     credentials: true,
   })
 );
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  next();
+});
 
 app.use(Express.json());
 
