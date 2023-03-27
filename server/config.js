642bb19405a8870995bf669187a333a38cff07a6
Pankaj Jangir <pankajjangir20@cse.iiitp.ac.in>
2023-03-27 20:33:24 +0530

    app

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 export default {
   port: 1337,
-  origin: "https://mym.onrender.com",
+  origin: "https://mym-omega.vercel.app",
   dbUrl:
     "mongodb+srv://Pankaj:hellomongo@cluster1.g5jsnol.mongodb.net/?retryWrites=true&w=majority",
 };
